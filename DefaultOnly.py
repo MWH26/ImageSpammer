de090ff06bc87b08e9753dc26dec2558c112d5c8
MWH26 <41759962+MWH26@users.noreply.github.com>
2019-03-26 23:20:00 +0000

    Added non customizable files

---
+++ DefaultOnly.py
@@ -0,0 +1,53 @@
+import os
+import shutil
+import urllib.request
+
+SubFolders = []
+
+## ImageLink = input("Choose an image url or type d for default: ")
+## if ImageLink == "d":
+ImageLink = "https://upload.wikimedia.org/wikipedia/commons/3/35/Beypazar%C4%B1_H%C4%B1rkatepe_K%C3%B6y_%C3%A7e%C5%9Fmesi.jpg"
+##    ImageLink = "http://i.kinja-img.com/gawker-media/image/upload/s--pgoQB_r6--/c_scale,f_auto,fl_progressive,q_80,w_800/1394453911150307111.jpg"
+
+## ImageName = input("Choose an image name or type d for default: ")
+## if ImageName == "d":
+ImageName = "Beypazarı_Hırkatepe_Köy_çeşmesi.png"
+##    ImageName = "Beypazari.png"
+
+## BaseDir = input("Choose a directory or type d for default: ")
+## if BaseDir == "d":
+BaseDir = "C://users//"
+
+def download_image():
+    print("Downloading the image...")
+    global ImageLink
+    global ImageName
+    urllib.request.urlretrieve(ImageLink,ImageName)
+
+def find_directories():
+    global BaseDir
+    global SubFolders
+    print("Looking for directories...")
+    for dirpath, dirs, files in os.walk(BaseDir):
+        print(dirpath)
+        SubFolders.append(dirpath)
+    print(SubFolders)
+
+def copy_images():
+    print("Copying image to directories...")
+    global SubFolders
+    global ImageName
+    for dir in SubFolders:
+        shutil.copyfile(ImageName, "{}/{}".format(dir, ImageName))
+        print('File created in {}.'.format(dir))
+
+def cleaning_up():
+    print("Cleaning up")
+    global ImageName
+    os.remove(ImageName)
+
+download_image()
+find_directories()
+copy_images()
+cleaning_up()
+
